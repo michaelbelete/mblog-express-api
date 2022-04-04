b78596699b1ec86f69495b1310454e717af3597e
michael belete <it.michael.belete@gmail.com>
2022-04-04 03:36:33 +0300

    finished the api

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import morgan from "morgan";
+import bodyParser from "body-parser";
 const app = express();
 dotenv.config();
 app.use(express.json());
+app.use(morgan("tiny"));
+app.use(bodyParser.json());
 
 export default app;
