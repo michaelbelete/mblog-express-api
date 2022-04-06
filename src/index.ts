5757c54c9883928c15ea17eaadfbcb443d46ec03
michael belete <it.michael.belete@gmail.com>
2022-04-06 03:49:05 +0300

    add static file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import app from "./utils/app";
-
+import express from "express";
 import userRouter from "./routers/users";
 import postRouter from "./routers/posts";
 import categoryRouter from "./routers/category";
 
 const port = process.env.PORT || 8000;
 
+app.use('/', express.static('public'))
 app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/categories", categoryRouter);
+
 app.listen(port, () => {
   console.log(`
 🚀 Server ready at: http://localhost:${port}`);
